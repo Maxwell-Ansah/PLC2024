e31327c120b09dfed76d8b7ac7d5da7cf06a1f06
Maxwell Ansah <220176961@aston.ac.uk>
2024-01-30 14:06:13 +0000

    modified lab2

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,9 +1,14 @@
 --create inpFunc
-inpFunc = [1..5] 
+-- inpFunc = [1..5] 
+inpFunc a b = [a..b]
 
 --Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
+-- applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
+-- Guards
+-- applicatorFunc :: (Ord a, Num a) => a -> Int
+applicatorFunc inpFunc a b s    | s == 's' = sum (inpFunc a b)
+                                | otherwise = (sum (inpFunc a b))/b
 
 main = do
-    let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
+    let result = applicatorFunc inpFunc 1 20 's' --Call applicatorFunc with inpFunc and 'a' as args
     putStrLn("sum = " ++ show(result))
--- lab2-functionalprimer/python3/func_as_arg.py
+++ lab2-functionalprimer/python3/func_as_arg.py
@@ -1,10 +1,17 @@
-def listFunc():
-    return [i for i in range(1, 6)] #Create list of ints from 1 to 5, Haskell equivalent [1..5]
+def listFunc(a, b):
+    # return [i for i in range(1, 6)] #Create list of ints from 1 to 5, Haskell equivalent [1..5]
+    return [i for i in range(a, b)]
 
-def applicatorFunc(inpFunc, s):
+# def applicatorFunc(inpFunc, s):
+#     if s=='s':
+#         return sum(inpFunc())
+#     else:
+#         return sum(inpFunc())/5
+
+def applicatorFunc(inpFunc, a, b, s):
     if s=='s':
-        return sum(inpFunc())
+        return sum(inpFunc(a, b))
     else:
-        return sum(inpFunc())/5
+        return sum(inpFunc(a, b))/b
 
-print(applicatorFunc(listFunc, 's'))
+print(applicatorFunc(listFunc, 1, 6, 's'))
